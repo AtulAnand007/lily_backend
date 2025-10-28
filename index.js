785cbbac3870d0fb1ca99bf6902c46414687183b
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2025-10-28 11:16:21 +0530

    Setup express server and environment configuration

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from 'express';
-import env from 'dotenv'
+import dotenv from 'dotenv'
 import bodyParser from 'body-parser';
+import logger from './config/logger.js';
 
+
+// env configuration 
+dotenv.config();
 
 const app = express();
+
+
+
+
+// server setup 
+const port = process.env.PORT || 8001;
+app.listen(port, () => {
+    logger.info(`Server is running on port ${port}`);
+})
