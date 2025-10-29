cc331d17e2446872b4c6d5584be05b1760cc290e
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2025-10-29 11:36:45 +0530

    Added refister functionallity

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,42 @@
+import User from "../models/user.model.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import logger from "../config/logger.js";
+import { generateAndSendOTP, } from "../services/otp.service.js";
+
+// register
+export const registerUser = async(req, res) => {
+    try {
+
+        const { fullName, email, password } = req.body;
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+        });
+
+
+        await generateAndSendOTP(user);
+
+        res.json({ message: "Password reset OTP sent to your email" });
+
+
+    } catch (error) {
+        logger.error("Register error", { message: error.message });
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
 // login 
 
 
