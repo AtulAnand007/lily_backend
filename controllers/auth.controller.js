cc4f7c73691f3e3cb45c9b453c771b9282370743
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2025-10-30 16:14:54 +0530

    implement user login with password validation , email verification check

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,11 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import logger from "../config/logger.js";
 import { generateAndSendOTP, verifyOTP } from "../services/otp.service.js";
+import {
+    generateAccessToken,
+    generateRefreshToken
 
+} from "../services/token.service.js";
 // register
 export const registerUser = async(req, res) => {
     try {
@@ -121,6 +125,52 @@ export const resendOtp = async(req, res) => {
     } 
     */
 // login 
+
+export const login = async(req, res) => {
+    try {
+
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(404).json({ message: " Invalid user" })
+        }
+
+        if (!user.isVerified) {
+            return res.status(403).json({ message: "Please verify your email first" });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+
+        }
+
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+
+        logger.info("Login Successfull");
+
+        res.status(200).json({
+            message: "Login Successull",
+            user: user.toJSON(),
+            accessToken,
+            refreshToken
+        });
+
+    } catch (error) {
+        logger.error("Login failed", { message: error.message });
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+}
 
 
 // refresh
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, VerifyUserotp, resendOtp } from '../controllers/auth.controller.js';
+import { registerUser, VerifyUserotp, resendOtp, login } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/verifyOtp", VerifyUserotp);
 router.post("/resend-otp", resendOtp);
+router.post("/login", login);
 
 export default router;
