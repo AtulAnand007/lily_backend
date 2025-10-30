700993e1e784b4d80989a7fcd8f83060133e2ef8
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2025-10-30 12:03:14 +0530

    feat(auth): add user OTP verification route and controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import logger from "../config/logger.js";
-import { generateAndSendOTP, } from "../services/otp.service.js";
+import { generateAndSendOTP, verifyOTP } from "../services/otp.service.js";
 
 // register
 export const registerUser = async(req, res) => {
@@ -28,14 +28,41 @@ export const registerUser = async(req, res) => {
 
         await generateAndSendOTP(user);
 
-        res.json({ message: "OTP send to your email for verification" });
+        return res.status(200).json({ message: "OTP send to your email for verification" });
 
 
     } catch (error) {
         logger.error("Register failed", { message: error.message });
-        res.status(500).json({ message: " Internal Server error" });
+        return res.status(500).json({ message: " Internal Server error" });
     }
 }
+
+// otp verification 
+export const VerifyUserotp = async(req, res) => {
+    try {
+        const { email, otp } = req.body;
+
+        if (!email || !otp) {
+            return res.status(400).json({ message: "Email and OTP are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isOtpMatched = await verifyOTP(user, otp);
+        if (!isOtpMatched) {
+            return res.status(400).json({ message: "Invalid or expired OTP" });
+        }
+
+        logger.info(` Email verified successfully for ${email}`);
+        return res.status(200).json({ message: "Email verification successful" });
+    } catch (error) {
+        logger.error("OTP verification failed", { message: error.message });
+        return res.status(500).json({ message: "OTP verification failed" });
+    }
+};
 
 // login 
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/auth.controller.js';
+import { registerUser, VerifyUserotp } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
 
 
 router.post("/register", registerUser);
-
+router.post("/verifyOtp", VerifyUserotp);
 export default router;
