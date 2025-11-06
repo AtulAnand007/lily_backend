668c555624bdc6e69a6a83286cf90e76867ce7e7
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2025-11-06 19:48:13 +0530

    Added delete user functionallity

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,10 +1,10 @@
 import User from "../models/user.model.js"
-
+import cloudinary from "../config/cloudinary.js"
 
 export const getAllUser = async(req, res) => {
     try {
 
-        if (req.user.role != 'ADMIN') {
+        if (req.user.role !== 'ADMIN') {
             return res.status(403).json({ message: "Access denied: Admins only" });
         }
 
@@ -27,3 +27,37 @@ export const getAllUser = async(req, res) => {
 
     }
 };
+
+
+export const deleteUser = async(req, res) => {
+    try {
+
+        if (req.user.role !== "ADMIN") {
+            return res.status(403).json({ message: "Access denied: Admins only" });
+        }
+
+        const { userId } = req.params;
+        const user = await User.findById({ userId });
+
+        if (user.profileImagePublicId) {
+            await cloudinary.uploader.destroy(user.profileImagePublicId);
+        }
+
+        await user.deleteOne();
+        logger.info(`User deleted successfully: ${user.email}`);
+        return res.status(200).json({
+            success: true,
+            message: "User deleted successfully",
+        });
+
+
+    } catch (error) {
+
+        logger.error("Failed to delete user", { message: error.message });
+        return res.status(500).json({
+            success: false,
+            message: "Failed to delete user",
+        });
+
+    }
+}
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { authenticate, authorizeRoles } from "../middlewares/auth.middleware.js";
 const router = express.Router();
-import { getAllUser } from "../controllers/admin.controller.js";
+import { getAllUser, deleteUser } from "../controllers/admin.controller.js";
 
 
 router.get("/all", authenticate, authorizeRoles("ADMIN"), getAllUser);
+router.delete("/:userId", authenticate, authorizeRoles("ADMIN"), deleteUser);
 export default router;
