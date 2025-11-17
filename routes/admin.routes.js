3084a62f827f6144b4f34b80c655ca9504f3a45d
AtulAnand007 <anandatul089@gmail.com>
2025-11-17 19:41:42 +0530

    improved admin route with best practices

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,37 +1,58 @@
-import express from "express"
-import { authenticate, authorizeRoles } from "../middlewares/auth.middleware.js";
+import express from "express";
+import {
+  authenticate,
+  authorizeRoles,
+} from "../middlewares/auth.middleware.js";
 const router = express.Router();
 import {
-    getAllUser,
-    deleteUser,
-    getUserdetail,
-    getDashboardOverview
-
+  getAllUser,
+  deleteUser,
+  getUserdetail,
+  getDashboardOverview,
 } from "../controllers/admin.controller.js";
-import { createProduct, deleteProduct, updateProduct } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
+// middleware use
+router.use(authenticate);
+router.use(authorizeRoles("ADMIN"));
+
 // admin to user
-router.get("/all", authenticate, authorizeRoles("ADMIN"), getAllUser);
-router.get("/:userId", authenticate, authorizeRoles("ADMIN"), getUserdetail);
-router.delete("/:userId", authenticate, authorizeRoles("ADMIN"), deleteUser);
+router.get("/all", getAllUser);
+router.get("/:userId", getUserdetail);
+router.delete("/:userId", deleteUser);
 
 // admin to product
-
-router.post("/create", authenticate, authorizeRoles("ADMIN"),
-    async(req, res, next) => {
-        req.uploadType = "product_image",
-            next();
-    },
-    upload.single("image"), createProduct);
-router.put("/update-product/:id", authenticate, authorizeRoles("ADMIN"), async() => {
-    req.uploadType = "product_image"
+router.post(
+  "/create",
+  (req, res, next) => {
+    (req.uploadType = "product_image"), next();
+  },
+  upload.single("image"),
+  createProduct
+);
+router.put(
+  "/update-product/:id",
+  (req, res, next) => {
+    req.uploadType = "product_image";
     next();
-}, upload.single("image"), updateProduct);
-router.delete("/delete/:prodId", authenticate, authorizeRoles("ADMIN"), deleteProduct)
-
+  },
+  upload.single("image"),
+  updateProduct
+);
+router.delete(
+  "/delete/:prodId",
+  deleteProduct
+);
 
 // admin to admin
-router.get("/dashboard", authenticate, authorizeRoles("ADMIN"), getDashboardOverview);
+router.get(
+  "/dashboard",
+  getDashboardOverview
+);
 
 export default router;
