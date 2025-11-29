705be61ea52c65ab8ec8e29f53bfd8ed4e9ad320
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2025-11-29 10:02:15 +0530

    Added some changes

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { getProductById, updateProductImage } from '../controllers/product.controller.js';
-
+import { authenticate, authorizeRoles } from '../middlewares/auth.middleware.js';
+import { upload } from '../middlewares/multer.middleware.js';
 const router = express.Router();
 
+// product routes for all 
 router.get("/:id", getProductById);
+
+
+// product routes for admin
+router.use(authenticate);
+router.use(authorizeRoles("ADMIN"));
+
 router.put("/update-image/:id", async(req, res, next) => {
     req.uploadType = "product_image";
     next();
